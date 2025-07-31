2422489f126f94797c986a0f5e4e54b74c13d190
Yuri-Costa09 <yurircostawork@gmail.com>
2025-07-31 15:26:02 -0300

    chore: add 'users', 'book_sessions', and 'auth_jwt' to INSTALLED_APPS in settings.py

--- book_dogs/core/settings.py
+++ book_dogs/core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "users",
+    "book_sessions",
+    "auth_jwt",
 ]
 
 MIDDLEWARE = [
