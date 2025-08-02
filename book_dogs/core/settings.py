1d5c0dec9d57a15385eb93b5ed83ff9785c7cf43
Yuri-Costa09 <yurircostawork@gmail.com>
2025-08-01 23:54:17 -0300

    feat: implement user registration endpoint with UserRegisterView and update URL routing

--- book_dogs/core/settings.py
+++ book_dogs/core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "users",
     "book_sessions",
-    "auth_jwt",
 ]
 
 AUTH_USER_MODEL = "users.User"
--- book_dogs/users/views.py
+++ book_dogs/users/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import generics
+from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny
+from .models import User
 
 # Create your views here.
 
 
-class UserRegistrationView(APIView):
-    def post(self, request):
-        pass
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
