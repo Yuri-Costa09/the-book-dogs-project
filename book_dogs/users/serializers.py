69c4b19d2404ecc220eb3f9612503715923f2543
Yuri-Costa09 <yurircostawork@gmail.com>
2025-08-01 23:29:53 -0300

    feat: add UserRegistrationSerializer for user registration and update UserSerializer to include password field

--- book_dogs/users/serializers.py
+++ book_dogs/users/serializers.py
@@ -2,10 +2,27 @@ from rest_framework import serializers
 from .models import User, Profile
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ["id", "name", "email", "created_at", "updated_at"]
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {
+            "email": {"required": True},
+            "username": {"required": True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
 
 
 class ProfileSerializer(serializers.ModelSerializer):
