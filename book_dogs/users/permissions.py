1a122c1ee773414a2b86e0fbfbbba72b848ec3b1
Yuri-Costa09 <yurircostawork@gmail.com>
2025-08-05 21:39:39 -0300

    feat: add custom IsOwner permission and extend profile URL routing with update and delete endpoints

--- book_dogs/users/permissions.py
+++ book_dogs/users/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 
+# Plugging customized permission
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
--- book_dogs/users/urls.py
+++ book_dogs/users/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .views import UserRegisterView, CreateProfileView, ProfileDetailView
+from .views import (
+    UserRegisterView,
+    CreateProfileView,
+    ProfileDetailView,
+    UpdateProfileView,
+    DeleteProfileView,
+)
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("profile/", CreateProfileView.as_view(), name="profile"),
     path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+    path(
+        "profile/<int:pk>/update/", UpdateProfileView.as_view(), name="profile-update"
+    ),
+    path(
+        "profile/<int:pk>/delete/", DeleteProfileView.as_view(), name="profile-delete"
+    ),
 ]
